6d0b641ef23bd0982e15a9e728396736f692ead8
Jessé Sampaio <jessesampaio.ti@gmail.com>
2017-10-29 22:44:59 -0300

    Aula 03
    Utilizando menus
    -Criando um Floating Action Button
    -Navegando pelas telas com Intents
    -Utilizando menus

--- app/src/main/java/br/com/enterprise/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/enterprise/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.enterprise.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -12,11 +15,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Cristiano Ronaldo", "Messi", "Neymar", "Rivaldo", "Ronaldo", "Ronaldinho Gaucho", "Zidane"};
+        String[] alunos = {"Cristiano Ronaldo", "Daniel Alves","Gareth Frank Bale", "Maradona", "Marcelo","Messi", "Neymar", "Rivaldo", "Ronaldo", "Ronaldinho Gaucho", "Zidane"};
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos );
         listaAlunos.setAdapter(adapter);
 
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
 
     }
 
